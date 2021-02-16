0005e050cb3cdead5c06ddebaa8936e0e7ceb0a8
bartolbio <65822723+bartolbio@users.noreply.github.com>
2021-02-16 23:04:18 +0100

    Update install.sh

--- vm/install.sh
+++ vm/install.sh
@@ -8,4 +8,6 @@ reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 	#installa basi
 pacstrap /mnt base linux-firmware linux-lts nano 
 genfstab -U /mnt >> /mnt/etc/fstab
-
+mkdir /mnt/installscript
+cp -RT ./ /mnt/installscript
+arch-chroot /mnt /installscript/chroot.sh
